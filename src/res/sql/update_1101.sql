2847433b05c682d014e859b4879edb6da387d5d3
LM Garret <max.garret@wanadoo.fr>
2018-02-27 17:45:10 +0100

    Added NDS support

--- src/res/sql/update_1101.sql
+++ src/res/sql/update_1101.sql
@@ -5,4 +5,7 @@ INSERT OR REPLACE INTO Platform(id,name_key,is_pc,default_supported_extensions,i
 INSERT OR REPLACE INTO Platform(id,name_key,is_pc,default_supported_extensions,igdb_id) VALUES (22,"3ds",0,"3ds,3dsx,elf,axf,cci,cxi,app",37);
 INSERT OR REPLACE INTO Emulator(id,name,default_path,default_args_schema) VALUES (10,"Citra", "C:\Program Files\Citra\citra-qt.exe","%p");
 INSERT OR REPLACE INTO emulates(emu_id,platform_id) VALUES(10,22);
+INSERT OR REPLACE INTO Platform(id,name_key,is_pc,default_supported_extensions,igdb_id) VALUES (23,"ds",0,"nds,zip,7z,rar,gz",20);
+INSERT OR REPLACE INTO Emulator(id,name,default_path,default_args_schema) VALUES (11,"DeSmuME", "C:\Program Files\desmume-0.9.11-win32\DeSmuME_0.9.11_x86.exe","%p");
+INSERT OR REPLACE INTO emulates(emu_id,platform_id) VALUES(11,23),(5,23);
 COMMIT;
